a9a34d479f3aeadfd82c6c216e5375f9f0651404
ZUNbado <git@acs.li>
2015-01-28 17:31:10 +0100

    prettytables and requirements

--- onappapi.py
+++ onappapi.py
@@ -1,4 +1,6 @@
 import json
+from prettytable import PrettyTable
+import os
 import pycurl
 from StringIO import StringIO
 from vm import VM
@@ -8,11 +10,40 @@ class OnApp(object):
     username = None
     password = None
     url = None
+    tmpdir = '/tmp/onapp'
 
-    def __init__(self, username, password, url):
+    def __init__(self, username, password, url, tmpdir = '/tmp/onapp'):
         self.username = username
         self.password = password
         self.url = url
+
+    def get_data(self, url):
+        data = self.get_cache(url)
+        if data:
+            return data
+        else:
+            data = self.exec_url(url)
+            data = self.is_valid_out(data)
+            if data:
+                self.write_cache(url, data)
+                return data
+        return False
+
+    def write_cache(self, url, data):
+        fullpath = os.path.join(self.tmpdir, url)
+        if not os.path.exists(os.path.dirname(fullpath)): os.makedirs(os.path.dirname(fullpath))
+        fd = open(fullpath, 'w')
+        fd.write(json.dumps(data, indent=3))
+        fd.close()
+
+    def get_cache(self, url):
+        try:
+            fd = open(os.path.join(self.tmpdir, url), 'r')
+        except:
+            return False
+        data = fd.read()
+        if data: return json.loads(data)
+        else: return False
 
     def exec_url(self, url, method = 'GET'):
         buffer = StringIO()
@@ -34,21 +65,19 @@ class OnApp(object):
         if output['status'] == 200: return output['body']
         else: return False
 
-    def vm_list(self):
-        data = self.exec_url('virtual_machines.json')
-        data = self.is_valid_out(data)
+    def vm_list(self, sortby = 'Hostname'):
+        data = self.get_data('virtual_machines.json')
         if data:
+            pt = PrettyTable(['Hostname', 'VMID', 'CPU', 'RAM', 'Disk', 'Powered on'])
+            pt.align['Hostname'] = 'l'
             for virtual in data:
-                vm = virtual['virtual_machine']
+                vm = VM(virtual)
+                pt.add_row([vm.hostname, vm.id, vm.cpus, vm.memory, vm.total_disk_size, vm.booted])
 
-                vm_id = vm['id']
-                vm_name = vm['hostname']
-
-                print "id: %s\t\thostname: %s" % (vm_id, vm_name)
+            print pt.get_string(sortby=sortby)
 
     def vm_info(self, vm_id):
-        data = self.exec_url('virtual_machines/%s.json' % vm_id)
-        data = self.is_valid_out(data)
+        data = self.get_data('virtual_machines/%s.json' % vm_id)
         if data:
             vm = VM(data)
             print vars(vm)
@@ -66,5 +95,10 @@ class OnApp(object):
         data = self.is_valid_out(data)
 
         if data:
-            for t in data:
-                print vars(Template(t))
+            pt = PrettyTable(['Label', 'Version', 'OS', 'Virtualitzation'])
+            pt.align['Label'] = 'l'
+            for tdata in data:
+                t = Template(tdata)
+                pt.add_row([t.label, t.version, t.operating_system, t.virtualization])
+
+            print pt
